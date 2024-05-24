4b36b74b7f38fbeea4f8668ea94c960f3b760f57
pruthwidinde <168171714+pruthwidinde@users.noreply.github.com>
2024-05-24 16:03:59 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    
+}
+resource "aws_instance" "name" {
+    ami = "ami-0cc9838aa7ab1dce7"
+    instance_type = "t2.micro"
+    key_name = "pruthwi.key"
+    tags = {
+      Name ="hi"
+    }
+}
